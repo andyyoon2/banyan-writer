da3864876e7438e65f2779ab23c7956e43d1db15
Andy Yoon <783285+andyyoon2@users.noreply.github.com>
2025-10-10 14:44:39 -0400

    Write tests for updating node content

--- src/components/__tests__/BanyanContext.test.tsx
+++ src/components/__tests__/BanyanContext.test.tsx
@@ -1,7 +1,7 @@
 // Tests which assert that when its public interface is used, BanyanContext transforms the tree in the expected ways
 
 import { renderHook } from "@solidjs/testing-library";
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { type BanyanDocument, createBanyanNode } from "../../data/tree";
 import {
 	BanyanProvider,
@@ -9,41 +9,95 @@ import {
 	useBanyanContext,
 } from "../BanyanContext";
 
-test("adds new child node under the given node", () => {
-	const root = createBanyanNode("root");
-	const initialStore = {
-		documents: [{ root } as BanyanDocument],
-	} satisfies BanyanStore;
+describe("addChildNode", () => {
+	test("adds new child node under the given node", () => {
+		const root = createBanyanNode("root");
+		const initialStore = {
+			documents: [{ root } as BanyanDocument],
+		} satisfies BanyanStore;
 
-	const { result } = renderHook(() => useBanyanContext(), {
-		wrapper: (props) => (
-			<BanyanProvider initialStore={initialStore}>
-				{props.children}
-			</BanyanProvider>
-		),
+		const { result } = renderHook(() => useBanyanContext(), {
+			wrapper: (props) => (
+				<BanyanProvider initialStore={initialStore}>
+					{props.children}
+				</BanyanProvider>
+			),
+		});
+
+		const { store, addChildNode } = result;
+		expect(store.documents[0].root.children.length).toBe(0);
+
+		addChildNode(root.id);
+		expect(store.documents[0].root.children.length).toBe(1);
 	});
 
-	const { store, addChildNode } = result;
-	expect(store.documents[0].root.children.length).toBe(0);
+	test("errors if given invalid parent ID", () => {
+		const root = createBanyanNode("root");
+		const initialStore = {
+			documents: [{ root } as BanyanDocument],
+		} satisfies BanyanStore;
 
-	addChildNode(root.id);
-	expect(store.documents[0].root.children.length).toBe(1);
+		const { result } = renderHook(() => useBanyanContext(), {
+			wrapper: (props) => (
+				<BanyanProvider initialStore={initialStore}>
+					{props.children}
+				</BanyanProvider>
+			),
+		});
+
+		const { addChildNode } = result;
+		expect(() => addChildNode("invalid")).toThrowError();
+	});
 });
 
-test("errors if given invalid parent ID", () => {
-	const root = createBanyanNode("root");
-	const initialStore = {
-		documents: [{ root } as BanyanDocument],
-	} satisfies BanyanStore;
+describe("handleNodeChange", () => {
+	test("updates node content", () => {
+		const root = createBanyanNode("root");
+		const initialStore = {
+			documents: [{ root } as BanyanDocument],
+		} satisfies BanyanStore;
 
-	const { result } = renderHook(() => useBanyanContext(), {
-		wrapper: (props) => (
-			<BanyanProvider initialStore={initialStore}>
-				{props.children}
-			</BanyanProvider>
-		),
+		const { result } = renderHook(() => useBanyanContext(), {
+			wrapper: (props) => (
+				<BanyanProvider initialStore={initialStore}>
+					{props.children}
+				</BanyanProvider>
+			),
+		});
+
+		// TODO: I shouldn't be testing by indexing documents. Should expose a getter from the context and use that.
+		const { store, handleNodeChange } = result;
+		expect(store.documents[0].root.content).toEqual("root");
+		handleNodeChange(root.id, "new content");
+		expect(store.documents[0].root.content).toEqual("new content");
 	});
 
-	const { addChildNode } = result;
-	expect(() => addChildNode("invalid")).toThrowError();
+	test("updates a node's content without affecting its parents", () => {
+		const root = createBanyanNode("parent content");
+		const child = createBanyanNode("child content");
+		root.children.push(child);
+
+		const initialStore = {
+			documents: [{ root } as BanyanDocument],
+		} satisfies BanyanStore;
+
+		const { result } = renderHook(() => useBanyanContext(), {
+			wrapper: (props) => (
+				<BanyanProvider initialStore={initialStore}>
+					{props.children}
+				</BanyanProvider>
+			),
+		});
+
+		const { store, handleNodeChange } = result;
+		expect(store.documents[0].root.children[0].content).toEqual(
+			"child content",
+		);
+
+		handleNodeChange(child.id, "new child content");
+		expect(store.documents[0].root.children[0].content).toEqual(
+			"new child content",
+		);
+		expect(store.documents[0].root.content).toEqual("parent content");
+	});
 });
