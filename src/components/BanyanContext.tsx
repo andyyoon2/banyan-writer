0ec18881ae0d5b596ae2e2f4b31f7990ce69603b
Andy Yoon <783285+andyyoon2@users.noreply.github.com>
2025-11-20 23:35:29 -0800

    Title editor

--- src/components/BanyanContext.tsx
+++ src/components/BanyanContext.tsx
@@ -21,26 +21,27 @@ export interface BanyanStore {
 }
 
 export interface BanyanContextType {
-	store: BanyanStore;
-	activeDoc: () => BanyanDocument | null;
-	setActiveDocIndex: (index: number) => void;
-	addDocument: () => void;
-	deleteDocument: (id: string) => void;
-	addChildNode: (parentId: string, content?: string) => void;
-	handleNodeChange: (id: string, content: string) => void;
-	moveNode: (props: {
-		sourceParentId: string;
-		sourceIndex: number;
-		targetParentId: string;
-		targetIndex: number;
-	}) => void;
-	deleteNode: (props: { parentId: string; index: number }) => void;
+  store: BanyanStore;
+  activeDoc: () => BanyanDocument | null;
+  setActiveDocIndex: (index: number) => void;
+  addDocument: () => void;
+  editDocumentTitle: (title: string) => void;
+  deleteDocument: (id: string) => void;
+  addChildNode: (parentId: string, content?: string) => void;
+  handleNodeChange: (id: string, content: string) => void;
+  moveNode: (props: {
+    sourceParentId: string;
+    sourceIndex: number;
+    targetParentId: string;
+    targetIndex: number;
+  }) => void;
+  deleteNode: (props: { parentId: string; index: number }) => void;
 }
 
 interface BanyanProviderProps {
-	children: JSXElement;
-	// Initialize the store. By default, the provider reads the value from localStorage.
-	initialStore?: BanyanStore;
+  children: JSXElement;
+  // Initialize the store. By default, the provider reads the value from localStorage.
+  initialStore?: BanyanStore;
 }
 
 const LOCAL_STORAGE_KEY = "banyan_documents";
@@ -49,212 +50,224 @@ const LOCAL_STORAGE_KEY = "banyan_documents";
 // useBanyanContext
 
 const BanyanContext = createContext<BanyanContextType>({
-	store: { documents: [] },
-	activeDoc: () => null,
-	setActiveDocIndex: () => {},
-	addDocument: () => {},
-	deleteDocument: () => {},
-	addChildNode: () => {},
-	handleNodeChange: () => {},
-	moveNode: () => {},
-	deleteNode: () => {},
+  store: { documents: [] },
+  activeDoc: () => null,
+  setActiveDocIndex: () => {},
+  addDocument: () => {},
+  editDocumentTitle: () => {},
+  deleteDocument: () => {},
+  addChildNode: () => {},
+  handleNodeChange: () => {},
+  moveNode: () => {},
+  deleteNode: () => {},
 });
 
 export const useBanyanContext = () => {
-	const context = useContext(BanyanContext);
-	if (!context) {
-		throw new Error("useBanyanContext must be used within a BanyanProvider");
-	}
-	return context;
+  const context = useContext(BanyanContext);
+  if (!context) {
+    throw new Error("useBanyanContext must be used within a BanyanProvider");
+  }
+  return context;
 };
 
 // --------------------------------------------------
 // PROVIDER
 
 export const BanyanProvider = (props: BanyanProviderProps) => {
-	// --------------------------------------------------
-	// Setup store with local storage syncing
+  // --------------------------------------------------
+  // Setup store with local storage syncing
 
-	let initialStore = props.initialStore;
-	if (!initialStore) {
-		let initialDocuments: BanyanDocument[];
-		try {
-			initialDocuments = localStorage.getItem(LOCAL_STORAGE_KEY)
-				? // biome-ignore lint/style/noNonNullAssertion: We know it exists
-					JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)!)
-				: [];
-		} catch (_error) {
-			initialDocuments = [];
-		}
+  let initialStore = props.initialStore;
+  if (!initialStore) {
+    let initialDocuments: BanyanDocument[];
+    try {
+      initialDocuments = localStorage.getItem(LOCAL_STORAGE_KEY)
+        ? // biome-ignore lint/style/noNonNullAssertion: We know it exists
+          JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)!)
+        : [];
+    } catch (_error) {
+      initialDocuments = [];
+    }
 
-		initialStore = { documents: initialDocuments };
-	}
+    initialStore = { documents: initialDocuments };
+  }
 
-	const [store, setStore] = createStore<BanyanStore>(initialStore);
-	const [activeDocIndex, setActiveDocIndex] = createSignal(0);
-	const activeDoc = () =>
-		store.documents.length ? store.documents[activeDocIndex()] : null;
+  const [store, setStore] = createStore<BanyanStore>(initialStore);
+  const [activeDocIndex, setActiveDocIndex] = createSignal(0);
+  const activeDoc = () =>
+    store.documents.length ? store.documents[activeDocIndex()] : null;
 
-	createEffect(() => {
-		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(store.documents));
-	});
+  createEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(store.documents));
+  });
 
-	// --------------------------------------------------
-	// Event handlers
+  // --------------------------------------------------
+  // Event handlers
 
-	const addDocument = () => {
-		const newDocument = createBanyanDocument();
+  const addDocument = () => {
+    const newDocument = createBanyanDocument();
 
-		setStore(
-			produce((draft) => {
-				draft.documents.push(newDocument);
-			}),
-		);
-	};
+    setStore(
+      produce((draft) => {
+        draft.documents.push(newDocument);
+      })
+    );
+  };
 
-	const deleteDocument = (id: string) => {
-		if (activeDocIndex() === store.documents.length - 1) {
-			setActiveDocIndex(Math.max(0, activeDocIndex() - 1));
-		}
-		setStore(
-			produce((draft) => {
-				draft.documents = draft.documents.filter((doc) => doc.id !== id);
-			}),
-		);
-	};
+  const editDocumentTitle = (title: string) => {
+    setStore(
+      "documents",
+      activeDocIndex(),
+      produce((activeDoc) => {
+        activeDoc.title = title;
+      })
+    );
+  };
 
-	const handleNodeChange = (id: string, content: string) => {
-		if (!store.documents.length) {
-			return;
-		}
+  const deleteDocument = (id: string) => {
+    if (activeDocIndex() === store.documents.length - 1) {
+      setActiveDocIndex(Math.max(0, activeDocIndex() - 1));
+    }
+    setStore(
+      produce((draft) => {
+        draft.documents = draft.documents.filter((doc) => doc.id !== id);
+      })
+    );
+  };
 
-		// Grab the active document, then use produce to update it
-		setStore(
-			"documents",
-			activeDocIndex(),
-			produce((activeDoc) => {
-				// Could optimize lookups by passing through indexes
-				const editedNode = findNodeById(id, activeDoc.root);
-				// TODO: Test for error
-				editedNode.content = content;
-				activeDoc.updatedAt = new Date().toISOString();
-			}),
-		);
-	};
+  const handleNodeChange = (id: string, content: string) => {
+    if (!store.documents.length) {
+      return;
+    }
 
-	const addChildNode = (parentId: string, content?: string) => {
-		if (!store.documents.length) {
-			return;
-		}
+    // Grab the active document, then use produce to update it
+    setStore(
+      "documents",
+      activeDocIndex(),
+      produce((activeDoc) => {
+        // Could optimize lookups by passing through indexes
+        const editedNode = findNodeById(id, activeDoc.root);
+        // TODO: Test for error
+        editedNode.content = content;
+        activeDoc.updatedAt = new Date().toISOString();
+      })
+    );
+  };
 
-		// Grab the active document, then use produce to update it
-		setStore(
-			"documents",
-			activeDocIndex(),
-			produce((activeDoc) => {
-				const parent = findNodeById(parentId, activeDoc.root);
-				if (!parent) {
-					throw new Error("Invalid parent id.");
-				}
-				const child = createBanyanNode(content);
-				parent.children.push(child);
-			}),
-		);
-	};
+  const addChildNode = (parentId: string, content?: string) => {
+    if (!store.documents.length) {
+      return;
+    }
 
-	/** Move a child node from the source parent to the target parent, inserting it at the given index. */
-	const moveNode = ({
-		sourceParentId,
-		sourceIndex,
-		targetParentId,
-		targetIndex,
-	}: {
-		sourceParentId: string;
-		sourceIndex: number;
-		targetParentId: string;
-		targetIndex: number;
-	}) => {
-		if (
-			!sourceParentId ||
-			sourceIndex < 0 ||
-			!targetParentId ||
-			targetIndex < 0 ||
-			!store.documents.length
-		) {
-			return;
-		}
+    // Grab the active document, then use produce to update it
+    setStore(
+      "documents",
+      activeDocIndex(),
+      produce((activeDoc) => {
+        const parent = findNodeById(parentId, activeDoc.root);
+        if (!parent) {
+          throw new Error("Invalid parent id.");
+        }
+        const child = createBanyanNode(content);
+        parent.children.push(child);
+      })
+    );
+  };
 
-		// Grab the active document, then use produce to update it
-		setStore(
-			"documents",
-			activeDocIndex(),
-			produce((activeDoc) => {
-				const sourceParent = findNodeById(sourceParentId, activeDoc.root);
-				if (!sourceParent) {
-					throw new Error("Invalid sourceParentId.");
-				}
-				const targetParent = findNodeById(targetParentId, activeDoc.root);
-				if (!targetParent) {
-					throw new Error("Invalid targetParentId.");
-				}
-				if (sourceIndex > sourceParent.children.length) {
-					throw new Error("sourceIndex out of bounds.");
-				}
-				if (targetIndex > targetParent.children.length) {
-					throw new Error("targetIndex out of bounds.");
-				}
+  /** Move a child node from the source parent to the target parent, inserting it at the given index. */
+  const moveNode = ({
+    sourceParentId,
+    sourceIndex,
+    targetParentId,
+    targetIndex,
+  }: {
+    sourceParentId: string;
+    sourceIndex: number;
+    targetParentId: string;
+    targetIndex: number;
+  }) => {
+    if (
+      !sourceParentId ||
+      sourceIndex < 0 ||
+      !targetParentId ||
+      targetIndex < 0 ||
+      !store.documents.length
+    ) {
+      return;
+    }
 
-				// Remove node from source parent and add it to target parent
-				const [sourceNode] = sourceParent.children.splice(sourceIndex, 1);
-				targetParent.children.splice(targetIndex, 0, sourceNode);
-			}),
-		);
-	};
+    // Grab the active document, then use produce to update it
+    setStore(
+      "documents",
+      activeDocIndex(),
+      produce((activeDoc) => {
+        const sourceParent = findNodeById(sourceParentId, activeDoc.root);
+        if (!sourceParent) {
+          throw new Error("Invalid sourceParentId.");
+        }
+        const targetParent = findNodeById(targetParentId, activeDoc.root);
+        if (!targetParent) {
+          throw new Error("Invalid targetParentId.");
+        }
+        if (sourceIndex > sourceParent.children.length) {
+          throw new Error("sourceIndex out of bounds.");
+        }
+        if (targetIndex > targetParent.children.length) {
+          throw new Error("targetIndex out of bounds.");
+        }
 
-	const deleteNode = ({
-		parentId,
-		index,
-	}: {
-		parentId: string;
-		index: number;
-	}) => {
-		if (!store.documents.length || !parentId || index < 0) {
-			return;
-		}
-		setStore(
-			"documents",
-			activeDocIndex(),
-			produce((activeDoc) => {
-				const parent = findNodeById(parentId, activeDoc.root);
-				if (!parent) {
-					throw new Error("Invalid parentId.");
-				}
-				if (index > parent.children.length) {
-					throw new Error("index out of bounds.");
-				}
-				parent.children.splice(index, 1);
-			}),
-		);
-	};
+        // Remove node from source parent and add it to target parent
+        const [sourceNode] = sourceParent.children.splice(sourceIndex, 1);
+        targetParent.children.splice(targetIndex, 0, sourceNode);
+      })
+    );
+  };
 
-	// --------------------------------------------------
-	// Return context value
+  const deleteNode = ({
+    parentId,
+    index,
+  }: {
+    parentId: string;
+    index: number;
+  }) => {
+    if (!store.documents.length || !parentId || index < 0) {
+      return;
+    }
+    setStore(
+      "documents",
+      activeDocIndex(),
+      produce((activeDoc) => {
+        const parent = findNodeById(parentId, activeDoc.root);
+        if (!parent) {
+          throw new Error("Invalid parentId.");
+        }
+        if (index > parent.children.length) {
+          throw new Error("index out of bounds.");
+        }
+        parent.children.splice(index, 1);
+      })
+    );
+  };
 
-	const banyanContextValue = {
-		store,
-		activeDoc,
-		setActiveDocIndex,
-		addDocument,
-		deleteDocument,
-		addChildNode,
-		handleNodeChange,
-		moveNode,
-		deleteNode,
-	} satisfies BanyanContextType;
+  // --------------------------------------------------
+  // Return context value
 
-	return (
-		<BanyanContext.Provider value={banyanContextValue}>
-			{props.children}
-		</BanyanContext.Provider>
-	);
+  const banyanContextValue = {
+    store,
+    activeDoc,
+    setActiveDocIndex,
+    addDocument,
+    editDocumentTitle,
+    deleteDocument,
+    addChildNode,
+    handleNodeChange,
+    moveNode,
+    deleteNode,
+  } satisfies BanyanContextType;
+
+  return (
+    <BanyanContext.Provider value={banyanContextValue}>
+      {props.children}
+    </BanyanContext.Provider>
+  );
 };
--- src/components/DocumentTitleControls.tsx
+++ src/components/DocumentTitleControls.tsx
@@ -1,19 +1,51 @@
+import { createSignal, Show } from "solid-js";
 import { getDocumentTitle } from "../utils";
 import { useBanyanContext } from "./BanyanContext";
 import { Button } from "./Button";
 
 export function DocumentTitleControls() {
-	const { activeDoc, deleteDocument } = useBanyanContext();
+  const { activeDoc, editDocumentTitle, deleteDocument } = useBanyanContext();
+  const title = () => getDocumentTitle(activeDoc());
+  const [editing, setEditing] = createSignal(false);
+  const [editedTitle, setEditedTitle] = createSignal(title());
 
-	if (!activeDoc()) {
-		return null;
-	}
+  if (!activeDoc()) {
+    return null;
+  }
 
-	return (
-		<div class="flex flex-row gap-2">
-			<strong class="font-semibold">{getDocumentTitle(activeDoc())}</strong>
-			{/* TODO: Once deleted, the active document needs to be reset. The index resetting isn't working currently. */}
-			<Button onClick={() => deleteDocument(activeDoc().id)}>Delete</Button>
-		</div>
-	);
+  const handleCancel = () => {
+    setEditing(false);
+    setEditedTitle(title());
+  };
+
+  const handleSave = () => {
+    editDocumentTitle(editedTitle());
+    setEditing(false);
+    setEditedTitle(title());
+  };
+
+  return (
+    <div class="flex flex-row gap-2">
+      <Show
+        when={editing()}
+        fallback={
+          <>
+            <strong class="font-semibold">{title()}</strong>
+            <Button onClick={() => setEditing(!editing())}>Edit</Button>
+          </>
+        }
+      >
+        <input
+          type="text"
+          value={editedTitle()}
+          onChange={(event) => setEditedTitle(event.target.value)}
+        />
+        <Button onClick={handleCancel}>Cancel</Button>
+        <Button onClick={handleSave}>Save</Button>
+      </Show>
+
+      {/* TODO: Once deleted, the active document needs to be reset. The index resetting isn't working currently. */}
+      <Button onClick={() => deleteDocument(activeDoc().id)}>Delete</Button>
+    </div>
+  );
 }
